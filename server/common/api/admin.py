bcc0e5b1290a2b6dafaaadf69bb43b28753cfa36
YoungsunCho <cyssun@kookmin.ac.kr>
2019-01-16 10:27:25 +0900

    split Image table from Class table

--- server/common/api/admin.py
+++ server/common/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Class)
+admin.site.register(Reservation)
+admin.site.register(TimeTable)
+admin.site.register(Image)
