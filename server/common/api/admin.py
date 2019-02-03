49288393707897d04d6d60489ecb9d24a6f50a87
YoungsunCho <cyssun@kookmin.ac.kr>
2019-02-03 20:36:14 +0900

    Revert admin.py file

--- server/common/api/admin.py
+++ server/common/api/admin.py
@@ -1,3 +1,9 @@
+from .models import *
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Class)
+admin.site.register(Reservation)
+admin.site.register(TimeTable)
+admin.site.register(Image)
+admin.site.register(InviteCode)
