a646560fd9b6e42bf310a74e4b75186adb6b7223
YoungsunCho <cyssun@kookmin.ac.kr>
2019-02-05 15:46:51 +0900

    revert InviteFriendsActivity file

--- Android/app/src/main/java/uncommon/common/activity/InviteFriendsActivity.java
+++ Android/app/src/main/java/uncommon/common/activity/InviteFriendsActivity.java
@@ -1,25 +1,11 @@
 package uncommon.common.activity;
 
-import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import uncommon.common.R;
 
 public class InviteFriendsActivity extends AppCompatActivity {
-
-    EditText authEmail;
-    Button authBtn;
-
-    LayoutInflater dialog; //LayoutInflater
-    View dialogLayout; //layout을 담을 View
-    Dialog authDialog; //dialog 객체
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
